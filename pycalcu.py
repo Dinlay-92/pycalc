e63b46229ccd90aad81f03c87d6b0af52f5dad77
Laydin Aboudou-Mze <22502947@me.uvsq.fr>
2026-02-26 11:40:42 +0100

    Ajout de l'opérateur multipliation et amelioration de l'interface

--- pycalcu.py
+++ pycalcu.py
@@ -1,7 +1,12 @@
 def calculator():
-	a = int(input("Veuillez choisir la premiere valeur"))
-	b = int(input("Veuillez choisir la deuxieme valeur"))
-	somme = a+b
-	print(somme)
+	a = int(input("Veuillez choisir la premiere valeur : \n"))
+	b = int(input("Veuillez choisir la deuxieme valeur : \n"))
+	fonction = input("Veuillez chosir le mode opérateur :\n s(somme) \n m(multiplication) \n choix :")
+	if fonction == "s" :
+		print("resultat : ",a+b)
+	elif fonction == "m" :
+		print("resulat : ",a*b)
+	else:
+		print("Opérateur inexistant")
 
 calculator()
