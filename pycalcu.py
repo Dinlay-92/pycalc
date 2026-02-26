02578716c62bfa42f4b17073ece2921b008326de
Laydin Aboudou-Mze <22502947@me.uvsq.fr>
2026-02-26 11:47:35 +0100

    Ajout de l'opérateur division

--- pycalcu.py
+++ pycalcu.py
@@ -1,11 +1,13 @@
 def calculator():
 	a = int(input("Veuillez choisir la premiere valeur : \n"))
 	b = int(input("Veuillez choisir la deuxieme valeur : \n"))
-	fonction = input("Veuillez chosir le mode opérateur :\n s(somme) \n m(multiplication) \n choix :")
+	fonction = input("Veuillez chosir le mode opérateur :\n s(somme) \n m(multiplication) \n d(division) \n choix :")
 	if fonction == "s" :
 		print("resultat : ",a+b)
 	elif fonction == "m" :
 		print("resulat : ",a*b)
+	elif fonction == "d" :
+		print("resulat : ",a/b)
 	else:
 		print("Opérateur inexistant")
 
