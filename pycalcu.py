3e662a40de6526bf7dd75da44b8c35858f407c18
Laydin Aboudou-Mze <22502947@me.uvsq.fr>
2026-02-26 11:49:43 +0100

    Ajout de l'operateur différence

--- pycalcu.py
+++ pycalcu.py
@@ -1,13 +1,15 @@
 def calculator():
 	a = int(input("Veuillez choisir la premiere valeur : \n"))
 	b = int(input("Veuillez choisir la deuxieme valeur : \n"))
-	fonction = input("Veuillez chosir le mode opérateur :\n s(somme) \n m(multiplication) \n d(division) \n choix :")
+	fonction = input("Veuillez chosir le mode opérateur :\n s(somme) \n m(multiplication) \n d(division) \n di(difference) \n choix :")
 	if fonction == "s" :
 		print("resultat : ",a+b)
 	elif fonction == "m" :
 		print("resulat : ",a*b)
 	elif fonction == "d" :
 		print("resulat : ",a/b)
+	elif fonction == "di" :
+		print("resulat : ",a-b)
 	else:
 		print("Opérateur inexistant")
 
